e6f41f9eff7b7af95641bafa7f63b7d5cf252333
noahfranco <noahfra@gmail.com>
2019-11-25 14:04:45 -0500

    woring on img

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,21 @@
 import React from "react";
- 
+import { homepage } from "../img/homepage.jpg"
 
 const Home = () => {
     
     return(
         <>
+        <div> 
+        <img src={homepage} alt="home photo" />
+        </div>
+        <div> 
         <h1> Home Page </h1>
+        </div> 
+        <div>
+        
+        </div> 
         </> 
-    )
+    );
 }
 
 export default Home; 
