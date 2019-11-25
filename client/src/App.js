44b0191a7982728248983f53940c1cf214499edc
efrain <efarinva98@gmail.com>
2019-11-25 14:42:35 -0500

    ready

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import Home from "./components/Home.js";
+// import Home from "./components/Home.js";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <div>
+      hello
       <Router> 
-      <Route exact path="/"  render={props => (
+      {/* <Route exact path="/"  render={props => (
         <Home { ...props } />
-      )} />  
+      )} />   */}
       </Router>    
     </div>
   );
--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {BrowserRouter as Router} from 'react-router-dom';
-import thunk from 'redux-redux';
+import thunk from 'redux-thunk';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import combineReducers from './Reducers';
