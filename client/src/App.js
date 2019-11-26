d94e715c86f9b5524d9ed4922ac76a471b41f118
noahfranco <noahfra@gmail.com>
2019-11-26 18:46:32 -0500

    working on home component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+// import "./styling/App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./components/Home.js"; 
 
