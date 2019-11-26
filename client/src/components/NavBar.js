dfb0b1bc5c7827821a9cb6d247336a018158d240
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-11-25 21:05:25 -0700

    built out the shell of the NavBar.
    Still need to add styles

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from "react"
+// import logo from
+
+import "./Navbar.scss"
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <a href="#">
+          <img src={logo} alt="Red Suppliers LLC Logo" />
+        </a>
+        <nav>
+          <a href="http://localhost:3000/">HOME</a>
+          <a href="http://localhost:3000/about">ABOUT</a>
+          <a href="http://localhost:3000/services">SERVICES</a>
+          <a href="http://localhost:3000/contactus">CONTACT US</a>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default NavBar
