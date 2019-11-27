a2fa5683b0d2dc8412f425f27f1dc33a96d754b1
noahfranco <noahfra@gmail.com>
2019-11-27 07:58:41 -0500

    working on spacing for our services section

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -18,9 +18,12 @@ const Home = () => {
         <div className="button" type="submit"> Contact US </div>
         </div> 
         <div className="spacing"> </div>
+        <div> 
+        <h2 style={{fontSize: "30px", display: "flex", justifyContent: "center"}}> Our Services </h2>
+        </div> 
         <div className="side-by-side"> 
            <div> 
-                <div> 
+                <div className=""> 
                <img className="handshake-img" src="https://www.maxpixel.net/static/photo/1x/Hands-Welcome-Handshake-Shaking-Hands-Agreement-3091908.jpg"/> 
                <div>    
                     <h3> Hand Shake </h3> 
@@ -34,10 +37,10 @@ const Home = () => {
             <div> 
             <div> 
                <img className="med-img" src="https://2001-2009.state.gov/cms_images/030405_med_supplies.jpg" /> 
-               </div>
                <div>    
                     <h3> Medical Supplier </h3> 
                 </div> 
+               </div>
                <div> 
                 <p> </p>
                </div>
