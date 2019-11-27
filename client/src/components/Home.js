40fb77bcaf15974d1dac5e5ba5cc3c3d0b27fdbf
noahfranco <noahfra@gmail.com>
2019-11-27 07:54:43 -0500

    working on our services section

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
         <img src="https://p0.piqsels.com/preview/4/544/846/medical-local-doctor-medicine.jpg" alt="home photo" />
         </div>
         <div> 
-        <h1> Red Suppliers LLC </h1>
+        <h1> Red Suppliers </h1>
         </div> 
         <div className="center"> 
         <p className="div1"> Red Suppliers specializes in fill the needs of government organizations by delivering needed products within the medical industry. Red Suppliers focuses on dental and portable aid supplies to be delivered to the needed locations </p> 
@@ -18,10 +18,13 @@ const Home = () => {
         <div className="button" type="submit"> Contact US </div>
         </div> 
         <div className="spacing"> </div>
-        <div className=""> 
+        <div className="side-by-side"> 
            <div> 
                 <div> 
-               <img className="small-img" src="https://www.maxpixel.net/static/photo/1x/Hands-Welcome-Handshake-Shaking-Hands-Agreement-3091908.jpg"/> 
+               <img className="handshake-img" src="https://www.maxpixel.net/static/photo/1x/Hands-Welcome-Handshake-Shaking-Hands-Agreement-3091908.jpg"/> 
+               <div>    
+                    <h3> Hand Shake </h3> 
+                </div> 
                </div>
                <div> 
                 <p> </p>
@@ -30,8 +33,11 @@ const Home = () => {
             <div className="spacing"> </div> 
             <div> 
             <div> 
-               <img className="small-img" src="https://2001-2009.state.gov/cms_images/030405_med_supplies.jpg" /> 
+               <img className="med-img" src="https://2001-2009.state.gov/cms_images/030405_med_supplies.jpg" /> 
                </div>
+               <div>    
+                    <h3> Medical Supplier </h3> 
+                </div> 
                <div> 
                 <p> </p>
                </div>
