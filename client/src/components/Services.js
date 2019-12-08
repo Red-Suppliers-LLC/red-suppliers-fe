68f4b156e1cee9b431de99649baf56f267d53c4b
noahfranco <noahfra@gmail.com>
2019-12-08 16:08:15 -0500

    fixed services compoenet responsive problem

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,46 +1,68 @@
 import React from "react"; 
 import Navbar from "./NavBar.js"; 
 import Footer from "./Footer.js"; 
+import "../styling/About.css"; 
 
 import "../styling/Home.css"; 
 
 const Services = () => {
 
     return (
-        <>
-        <div className="body"> 
-        <Navbar />
+        <div className="body">
+        <div>
+          <Navbar />
         </div>
-        <h2 className="center"> Our Services </h2>
-        <div className="double-space"> </div>
-        <div className="side-by-side"> 
-           <div className="services1"> 
-                <div> 
-               <img className="handshake-img" src="https://www.maxpixel.net/static/photo/1x/Hands-Welcome-Handshake-Shaking-Hands-Agreement-3091908.jpg"/> 
-               <div className="center">    
-                    <h3> Benefits </h3> 
-                </div>
-                <div className="benefits">
-                    <p className="font-size"> Through providing medical supplies we noticed that some buyers look for a few key things such as quality, efficiency, and low cost. In order to maintain consistency and quality we associate ourselves with other professionals to satisfy each and everyone of those criterias. One of our aims is to build a long term relationship of customer satisfaction. </p>    
-                </div>  
-               </div>
-            </div> 
-            <div className="spacing"> </div> 
-            <div className="services2">  
-               <img className="med-img" src="https://2001-2009.state.gov/cms_images/030405_med_supplies.jpg" /> 
-               <div className="center">    
-                    <h3> Medical Supplies </h3> 
-                </div> 
-               <div className="supplies"> 
-                <p className="font-size"> The list of products we supply can range from Basic First Aid to even OSHA, Pets, Coast Guard, Swimming First Aids and many more. We are constantly updating our inventory to satisfy our buyers. Contact us to see how we can provide what you may need. </p>
+  
+        <div className="spacing"> </div>
+  
+        <div  className="services-title"> 
+          <h2> Our Services </h2>
+          </div> 
+  
+          <div className="bit-spacing"> </div> 
+        <div className="center">
+       
+          <div className="spacing"> </div>
+  
+  
+  
+  
+          <div className="side-by-side"> 
+             <div className="services1"> 
+                  <div> 
+                 <img className="handshake-img" src="https://www.maxpixel.net/static/photo/1x/Hands-Welcome-Handshake-Shaking-Hands-Agreement-3091908.jpg"/> 
+                 <div className="center">    
+                      <h3 className="title-font"> Benefits </h3> 
+                  </div>
+                  <div className="center-width">
+                      <p className="font-size"> Through providing medical supplies we noticed that some buyers look for a few key things such as quality, efficiency, and low cost. In order to maintain consistency and quality we associate ourselves with other professionals to satisfy each and everyone of those criterias. One of our aims is to build a long term relationship of customer satisfaction. </p>   
+                  </div>  
+                 </div>
+              </div> 
+              <div className="spacing"> </div> 
+              <div className="services2">  
+                 <img className="med-img" src="https://2001-2009.state.gov/cms_images/030405_med_supplies.jpg" /> 
+                 <div className="wrapper">    
+                      <h3 className="title-font"> Medical Supplies </h3> 
+                  </div> 
+                 <div className="center-width"> 
+                  <p className="font-size"> The list of products we supply can range from Basic First Aid to even OSHA, Pets, Coast Guard, Swimming First Aids and many more. We are constantly updating our inventory to satisfy our buyers. Contact us to see how we can provide what you may need. </p>
+              </div>
             </div>
           </div>
+  
+          <div className="spacing"> </div>
         </div>
-        <div className="double-space"> </div>
-        <div className="center"> 
-            <Footer />
+        {/* our partners component */}
+        <div className="spacing"> </div>
+        <div>
+          {/* <OurPartners /> */}
+          <div className="spacing"> </div>
+          <div className="copy">
+              <Footer />     
+          </div>  
         </div>
-        </>
+      </div>
     );
 };
 
