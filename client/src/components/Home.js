fcee733ca731186ec5001f00e298becd4dd876f7
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-12-08 23:13:23 -0700

    attempted to make the image and nav bar responsive
    only touched the Home.js Home.css and Navbar.js and .css pages.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,23 +1,22 @@
 import React from "react"
 import "../styling/Home.css"
-import "../styling/About.css"; 
+import "../styling/About.css"
 
 import NavBar from "./NavBar"
-import HomeImg from "../img/homepageimgtwo.png";  
-import Footer from "./Footer.js";
-import Icons from "./Icons.js"; 
+import HomeImg from "../img/homepageimgtwo.png"
+import Footer from "./Footer.js"
+import Icons from "./Icons.js"
 
 const Home = () => {
-
   return (
     <div className="body">
       <div>
         <NavBar />
       </div>
-      <div>
+      <div className="home-img-div">
         <img className="home-img" src={HomeImg} alt="home" />
       </div>
-      <Icons /> 
+      <Icons />
       <div>
         <h1> Red Suppliers </h1>
       </div>
@@ -31,45 +30,65 @@ const Home = () => {
       </div>
 
       <div className="center">
-        <div className="button" type="submit" onClick={() => {}}> Contact Us </div>
-      </div>  
+        <div className="button" type="submit" onClick={() => {}}>
+          {" "}
+          Contact Us{" "}
+        </div>
+      </div>
 
       <div className="spacing"> </div>
 
-      <div  className="services-title"> 
+      <div className="services-title">
         <h2> Our Services </h2>
-        </div> 
+      </div>
 
-        <div className="bit-spacing"> </div> 
+      <div className="bit-spacing"> </div>
       <div className="center">
-     
         <div className="spacing"> </div>
-        <div className="side-by-side"> 
-           <div className="services1"> 
-                <div> 
-               <img className="handshake-img" src="https://www.maxpixel.net/static/photo/1x/Hands-Welcome-Handshake-Shaking-Hands-Agreement-3091908.jpg"/> 
-               <div className="center">    
-                    <h3 className="title-font"> Benefits </h3> 
-                </div>
-                <div className="center-width">
-                    <p className="font-size"> Through providing medical supplies we noticed that some buyers look for a few key things such as quality, efficiency, and low cost. In order to maintain consistency and quality we associate ourselves with other professionals to satisfy each and everyone of those criterias. One of our aims is to build a long term relationship of customer satisfaction. </p>   
-                </div>  
-               </div>
-            </div> 
-            <div className="spacing"> </div> 
-            <div className="services2">  
-               <img className="med-img" src="https://2001-2009.state.gov/cms_images/030405_med_supplies.jpg" /> 
-               <div className="wrapper">    
-                    <h3 className="title-font"> Medical Supplies </h3> 
-                </div> 
-               <div className="center-width"> 
-                <p className="font-size"> The list of products we supply can range from Basic First Aid to even OSHA, Pets, Coast Guard, Swimming First Aids and many more. We are constantly updating our inventory to satisfy our buyers. Contact us to see how we can provide what you may need. </p>
+        <div className="side-by-side">
+          <div className="services1">
+            <div>
+              <img
+                className="handshake-img"
+                src="https://www.maxpixel.net/static/photo/1x/Hands-Welcome-Handshake-Shaking-Hands-Agreement-3091908.jpg"
+              />
+              <div className="center">
+                <h3 className="title-font"> Benefits </h3>
+              </div>
+              <div className="center-width">
+                <p className="font-size">
+                  {" "}
+                  Through providing medical supplies we noticed that some buyers
+                  look for a few key things such as quality, efficiency, and low
+                  cost. In order to maintain consistency and quality we
+                  associate ourselves with other professionals to satisfy each
+                  and everyone of those criterias. One of our aims is to build a
+                  long term relationship of customer satisfaction.{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="spacing"> </div>
+          <div className="services2">
+            <img
+              className="med-img"
+              src="https://2001-2009.state.gov/cms_images/030405_med_supplies.jpg"
+            />
+            <div className="wrapper">
+              <h3 className="title-font"> Medical Supplies </h3>
+            </div>
+            <div className="center-width">
+              <p className="font-size">
+                {" "}
+                The list of products we supply can range from Basic First Aid to
+                even OSHA, Pets, Coast Guard, Swimming First Aids and many more.
+                We are constantly updating our inventory to satisfy our buyers.
+                Contact us to see how we can provide what you may need.{" "}
+              </p>
             </div>
           </div>
         </div>
-        <div> 
-            {/* <AboutUs /> */}
-        </div> 
+        <div>{/* <AboutUs /> */}</div>
         <div className="spacing"> </div>
       </div>
       {/* our partners component */}
@@ -78,8 +97,8 @@ const Home = () => {
         {/* <OurPartners /> */}
         <div className="spacing"> </div>
         <div className="copy">
-            <Footer />     
-        </div>  
+          <Footer />
+        </div>
       </div>
     </div>
   )
